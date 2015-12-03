0c8f71debcf9f63cd962ff0dd7aaff8049c79bc8
Tim Bart <tim@sayhello.com>
2015-12-03 14:13:28 -0800

    :lipstick:

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ python scripts for anomaly-detection
 # How to run
 
 1. Make sure Redis is running locally
-2. `python run.py configs/whatever.yml`
+2. `pip install -r requirements.txt`
+3. `python run.py configs/whatever.yml`
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
-sudo apt-get install git
-sudo apt-get install python-pip
-sudo apt-get install libpq-dev python-dev
-sudo apt-get install libblas-dev liblapack-dev
-sudo apt-get install gfortran-4.6
-sudo apt-get install libamd2.2.0 libblas3gf libc6 libgcc1
-sudo apt-get install libgfortran3 liblapack3gf libumfpack5.4.0 libstdc++6
-sudo apt-get install build-essential
-sudo apt-get install libatlas-base-dev
-sudo apt-get install gfortran
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo apt-get install -y libpq-dev python-dev
+sudo apt-get install -y libblas-dev liblapack-dev
+sudo apt-get install -y gfortran-4.6
+sudo apt-get install -y libamd2.2.0 libblas3gf libc6 libgcc1
+sudo apt-get install -y libgfortran3 liblapack3gf libumfpack5.4.0 libstdc++6
+sudo apt-get install -y build-essential
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y gfortran
