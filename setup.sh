dfac931bf215af78198a1e97a94a9446fcd7e965
Tim Bart <tim@sayhello.com>
2015-12-03 14:06:42 -0800

    packages required to use pip and for pip dependencies

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install python-pip
+sudo apt-get install libpq-dev python-dev
+sudo apt-get install libblas-dev liblapack-dev
+sudo apt-get install gfortran-4.6
+sudo apt-get install libamd2.2.0 libblas3gf libc6 libgcc1
+sudo apt-get install libgfortran3 liblapack3gf libumfpack5.4.0 libstdc++6
+sudo apt-get install build-essential
+sudo apt-get install libatlas-base-dev
+sudo apt-get install gfortran
