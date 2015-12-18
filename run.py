7d18552d8c26a65f4efcc4f22ff83c5a3dd4bba6
Jingyun <jingyunfan@gmail.com>
2015-12-17 17:59:43 -0800

    call method to store to account_questions, also added max_density parameter to dbscan_params (set equal to limit right now so alg_id stays the same)

--- run.py
+++ run.py
@@ -25,11 +25,13 @@ def main():
     while True:
         tracker = app.Tracker(config['redis'])
 
+        conn_common = psycopg2.connect(**config['common_db'])
         conn_sensors = psycopg2.connect(**config['sensors_db'])
         conn_anomaly = psycopg2.connect(**config['anomaly'])
 
         dbscan_params = config['dbscan_params']
-        
+        questions_config = config['questions']        
+
         account_ids = app.get_active_accounts(conn_sensors)
 
         for account_id in account_ids:
@@ -37,7 +39,7 @@ def main():
                 logging.debug("Skipping account: %d since we've already seen it", account_id)
                 continue
             
-            app.run(account_id, conn_sensors, conn_anomaly, dbscan_params)        
+            app.run(account_id, conn_common, conn_sensors, conn_anomaly, dbscan_params, questions_config)        
             tracker.track(account_id)
 
         logging.warn("Iteration done")
